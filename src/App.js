63e8eeca3b16d1a27862068dc41b9f28d7517459
Maneet Lodha <36129353+maneetlodha28@users.noreply.github.com>
2020-12-13 19:31:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Axios from "axios";
+import "./App.css";
 
-function App() {
+var fetchURL = "";
+const url = "https://the-cocktail-db.p.rapidapi.com/filter.php?i=";
+const App = () => {
+  const [drinks, setDrinks] = useState([]);
+
+  const setURL = (text) => {
+    fetchURL = url + text;
+    return fetchURL;
+  };
+
+  const clickhandler = async (text) => {
+    const response = await Axios.get(setURL(text), {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "b9cae0c425mshcf1d76cb7ab0306p141e00jsn2123b0d3da81",
+        "x-rapidapi-host": "the-cocktail-db.p.rapidapi.com",
+      },
+    });
+
+    console.log(response);
+    const data = response.data.drinks;
+
+    setDrinks(data);
+
+    // data.drinks.map((item, index) => {
+    //   console.log(item.strDrink);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1 style={{ textAlign: "center" }}>Cocktail Recommeder</h1>
+      <p style={{ fontStyle: "italic", textAlign: "center" }}>
+        find the best cocktails from around the world
+      </p>
+
+      <div>
+        <button
+          onClick={() => {
+            clickhandler("beer");
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Beer
+        </button>
+        <button
+          onClick={() => {
+            clickhandler("whiskey");
+          }}
+        >
+          Whiskey
+        </button>
+        <button
+          onClick={() => {
+            clickhandler("rum");
+          }}
+        >
+          Rum
+        </button>
+
+        <button
+          onClick={() => {
+            clickhandler("vodka");
+          }}
+        >
+          Vodka
+        </button>
+        <button
+          onClick={() => {
+            clickhandler("gin");
+          }}
+        >
+          Gin
+        </button>
+      </div>
+      <div className="row justify-content-center">
+        {drinks.map((drink, index) => {
+          return (
+            <div key={index} className="col-4 list">
+              <h6>
+                <img src={drink.strDrinkThumb} />
+              </h6>
+              {drink.strDrink}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
